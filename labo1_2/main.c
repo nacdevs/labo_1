37aebbcb6b4e8b05a028a765656a6f497560953e
Nestor <nestorcamela@gmail.com>
2018-08-30 20:30:08 -0300

    Funcion parametrizada

--- labo1_2/main.c
+++ labo1_2/main.c
@@ -2,32 +2,63 @@
 #include <stdio_ext.h>
 #include <stdlib.h>
 int getEdad();
+int getEntero(int* pEdad, int reintentos, char* msg, char* msgErr, int min, int max);
 int main()
 {
     int edad;
-    if(getEdad(&edad)==0){
+    /*if(getEdad(&edad)==0){
+        printf("La edad es %d",edad);
+    }*/
+
+    if(getEntero(&edad,3, "Edad?", "Error",0,199)){
         printf("La edad es %d",edad);
     }
     return 0;
 }
 
-int getEdad(int* pEdad){
+int getEntero(int* pEdad, int reintentos, char* msg, char* msgErr, int min, int max){
+
+    int retorno = -1;
+    int auxiliarEdad;
+
+    for(reintentos; reintentos>0; reintentos--){
+        printf("\nEdad?");
+        if(scanf("%d",&auxiliarEdad)==1){
+            if(auxiliarEdad >= min && auxiliarEdad < max){
+            *pEdad=auxiliarEdad;
+            retorno=0;
+            break;
+                }
+            }else{
+            printf(msgErr);
+            __fpurge(stdin);
+            }
+
+    }
+
+    return retorno;
+
+
+}
+
+
+/*int getEdad(int* pEdad){
 
     int reintentos=3;
     int retorno = -1;
     int auxiliarEdad;
 
 
-    for(reintentos=3; reintentos>0; reintentos--){
+    for(reintentos; reintentos>0; reintentos--){
         printf("\nEdad?");
         if(scanf("%d",&auxiliarEdad)==1){
-            if(auxiliarEdad >= 0 && auxiliarEdad < 199){
+            if(auxiliarEdad >= min && auxiliarEdad < max){
             *pEdad=auxiliarEdad;
             retorno=0;
             break;
                 }
             }else{
-            printf("La edad es numerica, debe ser mayor a 0 y menor a 199");
+            printf(msgErr);
             __fpurge(stdin);
             }
 
@@ -35,4 +66,4 @@ int getEdad(int* pEdad){
     }
 
     return retorno;
-}
+}*/
