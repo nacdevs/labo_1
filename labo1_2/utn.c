cd9be49c2903e8e81572534ead98eb90eac09803
Nestor <nestorcamela@gmail.com>
2018-08-30 21:09:37 -0300

    con lib y pdf para tp

--- labo1_2/utn.c
+++ labo1_2/utn.c
@@ -2,29 +2,11 @@
 #include <stdio_ext.h>
 #include <stdlib.h>
 #include "utn.h"
-int getEdad();
-//int getEntero(int* pEdad, int reintentos, char* msg, char* msgErr, int min, int max);
-int main()
-{
-    int edad;
-    /*if(getEdad(&edad)==0){
-        printf("La edad es %d",edad);
-    }*/
 
-    if(utn_getEntero(&edad,3, "Edad?", "Error",0,199)){
-        printf("La edad es %d",edad);
-    }
-    return 0;
-}
+int utn_getEntero(int* pEdad, int reintentos, char* msg, char* msgErr, int min, int max){
 
-
-
-/*int getEdad(int* pEdad){
-
-    int reintentos=3;
     int retorno = -1;
     int auxiliarEdad;
-
 
     for(reintentos; reintentos>0; reintentos--){
         printf("\nEdad?");
@@ -39,8 +21,9 @@ int main()
             __fpurge(stdin);
             }
 
-
     }
 
     return retorno;
-}*/
+
+
+}
